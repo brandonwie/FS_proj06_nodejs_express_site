ffb5730b6685c1c918823d414474f3e11b33ceac
Brandon Wie <brandonwie.cs@gmail.com>
2021-03-16 14:35:44 +0800

    FIX(app.js): remove deprecated body-parser, update pug/express

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 const express = require('express');
-const parser = require('express-parser');
 // launch Express
 const app = express();
-
-// app.use(bodyParser.json());
-app.use(parser.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }));
 
 // set static path
 app.use('/static', express.static('public'));
@@ -14,13 +11,13 @@ app.set('view engine', 'pug');
 const mainRoute = require('./routes');
 const aboutRoute = require('./routes/about');
 const projectRoute = require('./routes/projects');
+
 app.use(mainRoute);
 app.use('/about', aboutRoute);
 app.use('/projects', projectRoute);
 
 // Error
 app.use((req, res, next) => {
-<<<<<<< HEAD
 	const err = new Error('Not Found');
 	err.status = 404;
 	next(err);
@@ -32,21 +29,7 @@ app.use((err, req, res, next) => {
 	res.render('error');
 });
 
-app.listen(3000, console.log('The app is running on port 3000'));
-=======
-  const err = new Error("Not Found");
-  err.status = 404;
-  next(err);
-});
-
-app.use((err, req, res, next) => {
-  res.locals.error = err;
-  res.status(err.status);
-  res.render("error");
-});
-
 app.listen(
-  3000,
-  console.log("The app is running on port:3000 - localhost:3000")
+	3000,
+	console.log('The app is running on port:3000 - localhost:3000')
 );
->>>>>>> 605fe11fdc60762a5ec91c9260ab37b5a0342bd3
