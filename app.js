bbcec67d7a0d7c1fc9a133c7a77b19ac169cdb99
Brandon Wie <brandonwie.cs@gmail.com>
2021-03-17 15:21:39 +0800

    error fix trial

--- app.js
+++ app.js
@@ -30,6 +30,6 @@ app.use((err, req, res, next) => {
 	res.render('error');
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT | 3000, () => {
 	console.log('App listening on port 3000...');
 });
